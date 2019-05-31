ad4dbf94305d967ba6492771d5c071603469b05c
166001738 <513659413@qq.com>
2019-05-31 15:41:02 +0800

    订单栏增加公告
    从历史记录复制

--- miniprogram/pages/order/order.js
+++ miniprogram/pages/order/order.js
@@ -12,6 +12,7 @@ Page({
     itemnum:[{"number":0}],
     orderId: 0,
     item_number:[0,0,0],
+    logs:'',
   },
 
   /**
@@ -37,6 +38,29 @@ Page({
         console.error('[数据库] [查询记录] 失败：', err)
       }
     })
+       
+    // 1. 获取数据库引用
+    // 2. 构造查询语句
+    // collection 方法获取一个集合的引用
+    // where 方法传入一个对象，数据库返回集合中字段等于指定值的 JSON 文档。API 也支持高级的查询条件（比如大于、小于、in 等），具体见文档查看支持列表
+    // get 方法会触发网络请求，往数据库取数据
+    db.collection('anonuncement').where({
+        remark: "now"
+      }).get({
+        success: function (res) {
+          // 输出 [{ "title": "The Catcher in the Rye", ... }]
+          // console.log(res)
+          // console.log(res.data)
+          console.log(res.data[0].text)
+          // var text = res.data[0].text
+          console.log('===读取数据存入本地====')
+          that.setData({
+            logs: res.data[0].text
+          })
+          // that.data.logs= text
+          console.log(this.data[0].text)
+        }
+      })
 
     //获取openid
     wx.cloud.callFunction({

aa9bd1fca88a9f0f93d8f56ef7cdcf7a2fdca46a
166001738 <513659413@qq.com>
2019-06-02 16:33:33 +0800

    index on yun_function: ad4dbf9 订单栏增加公告

--- miniprogram/pages/order/order.js
+++ miniprogram/pages/order/order.js
@@ -12,9 +12,11 @@ Page({
     itemnum:[{"number":0}],
     orderId: 0,
     item_number:[0,0,0],
-    logs:'',
+    logs:'11',
   },
 
+ 
+  
   /**
    * 生命周期函数--监听页面加载
    */
@@ -44,23 +46,23 @@ Page({
     // collection 方法获取一个集合的引用
     // where 方法传入一个对象，数据库返回集合中字段等于指定值的 JSON 文档。API 也支持高级的查询条件（比如大于、小于、in 等），具体见文档查看支持列表
     // get 方法会触发网络请求，往数据库取数据
-    db.collection('anonuncement').where({
-        remark: "now"
-      }).get({
-        success: function (res) {
-          // 输出 [{ "title": "The Catcher in the Rye", ... }]
-          // console.log(res)
-          // console.log(res.data)
-          console.log(res.data[0].text)
-          // var text = res.data[0].text
-          console.log('===读取数据存入本地====')
-          that.setData({
-            logs: res.data[0].text
-          })
-          // that.data.logs= text
-          console.log(this.data[0].text)
-        }
-      })
+    db.collection('announcement').where({
+      remark: 'now'
+    }).get({
+      success: function (res) {
+        console.log(res)
+        // var text = res.data[0].text
+        console.log('===读取数据存入本地====')
+        that.setData({
+          logs: res.data[0].text
+        })
+        // that.data.logs= text
+        console.log(this.data[0].text)
+      },
+      fail(res) {
+        console.log(' 读取失败', res)
+      }
+    })
 
     //获取openid
     wx.cloud.callFunction({
